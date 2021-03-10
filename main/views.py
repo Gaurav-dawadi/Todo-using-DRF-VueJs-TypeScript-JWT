ec98be2ba528dd058ef72b2b667246e0c90580ee
Gaurav-dawadi <gaurabdawadi@gamil.com>
2021-03-10 16:02:46 +0545

    feat: add permission classes on views

--- main/views.py
+++ main/views.py
@@ -4,6 +4,7 @@ from django.contrib.auth.models import User
 from rest_framework.permissions import AllowAny
 from rest_framework.viewsets import ModelViewSet
 from rest_framework.generics import CreateAPIView
+from rest_framework.permissions import IsAuthenticated, AllowAny
 from rest_framework_simplejwt.views import TokenObtainPairView
 from .serializers import MyTokenObtainPairSerializer, RegisterUserSerializer
 
@@ -22,3 +23,4 @@ class RegisterUserView(CreateAPIView):
 class TodoViewSet(ModelViewSet):
     queryset = Todo.objects.all()
     serializer_class = TodoSerializer
+    permission_classes = (IsAuthenticated,)
