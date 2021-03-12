c068d93c9362239175be47b15f83d747395b2d5b
Gaurav-dawadi <gaurabdawadi@gamil.com>
2021-03-12 11:11:42 +0545

    feat: install axios and set API endpoint

---
+++ src/api/index.ts
@@ -0,0 +1,14 @@
+import axios from "axios";
+
+
+export const API = axios.create({
+    baseURL: "http://127.0.0.1:8000/api/"
+});
+
+export function setToken(jwt: string) {
+    API.defaults.headers.common["Authorization"] = `Bearer ${jwt}`;
+}
+
+export function clearToken() {
+    API.defaults.headers.common["Authorization"];
+}
